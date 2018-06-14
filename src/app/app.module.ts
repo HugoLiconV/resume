000df6bf8b942729453fed5e7355cec402bbf32e
Hugo <hugo_val28@hotmail.com>
2018-06-13 18:01:17 -0600

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeroComponent } from './hero/hero.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { AchievementsComponent } from './achievements/achievements.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    AchievementsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
