b08649f721e8636f25bceeb2f3b74637288e2ac5
Hugo <hugo_val28@hotmail.com>
2018-07-02 20:37:08 -0600

    added animation to logo

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as $ from 'jquery';
+import { ELEMENT_PROBE_PROVIDERS } from '@angular/platform-browser/src/dom/debug/ng_probe';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -10,6 +11,26 @@ export class NavbarComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const $nav = $('#logo-nav');
+    const $win = $(window);
+    let winH = $win.height(); // Get the window height.
+    let firstScroll = true;
+    $win.on('scroll', function () {
+      if ($(this).scrollTop() < winH) { //
+        if (firstScroll) {
+          $nav.addClass('hide');
+        } else {
+          $nav.removeClass('show-logo');
+          $nav.addClass('hide-logo');
+        }
+      } else {// esta en la primera seccion
+        $nav.removeClass('hide-logo');
+        $nav.addClass('show-logo');
+        firstScroll = false;
+      }
+    }).on('resize', function() { // If the user resizes the window
+       winH = $(this).height(); // you'll need the new height value
+    });
   }
 
 }
